73056e0dfbdc9a0db79d1812fff3899f45db73ae
unfinishedprogram <noah.labrecque@gmail.com>
2021-03-07 12:35:17 -0500

    Added many optomizations and better color comparison

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-
 cIn = document.getElementById("canvasIn");
-
 tempCan = document.getElementById('temp');
-
 img = document.getElementById("scream");
 
 const img_width = img.width;
 const img_height = img.height;
 
+stats = document.getElementById("stats");
+
+var testedRectangles = 0;
+var placedRectangles = 0;
 
 cIn.width = img_width;
 
@@ -25,64 +26,75 @@ ctxIn.drawImage(img, 0, 0);
 goalData = ctxIn.getImageData(0, 0, img_width, img_height);
 ctxIn.fillRect(0,0,img_width, img_height);
 
+function compareData(data, goal, rect){
+    let diff = 0;
+    let width = goal.width;
+    for (let x = 0; x < rect[2] && x + rect[0] < goal.width; x++) {
+        for (let y = 0; y < rect[3] && y + rect[1] < goal.height; y++) {
+            let datindex = (y * rect[2] + x) * 4;
+            let goalIndex = ((rect[1] + y) * width + (rect[0] + x)) *4;
 
-function compareData(data, goal){
-    let dif = 0;
-    for( let i = 0; i < data.data.length; i += 4 ){
+            rdiff = Math.abs(data.data[datindex + 0] - goal.data[goalIndex + 0]);
+            bdiff = Math.abs(data.data[datindex + 1] - goal.data[goalIndex + 1]);
+            gdiff = Math.abs(data.data[datindex + 2] - goal.data[goalIndex + 2]);
 
-        nextDif = Math.abs( data.data[i] - goal.data[i]);
-        if(nextDif)dif += nextDif;
-        nextDif = Math.abs( data.data[i+1] - goal.data[i+1]);
-        if(nextDif)dif += nextDif;
-        nextDif = Math.abs( data.data[i+2] - goal.data[i+2]);
-        if(nextDif)dif += nextDif;
+            diff += Math.sqrt(rdiff*rdiff + bdiff*bdiff + gdiff*gdiff);
+        }
     }
-    //console.log(dif/ data.data.length);
-    return dif / data.data.length;
+    return diff / (rect[2] * rect[3]);
 }
 
 function getRandomColor(){
     return `rgb(${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)})`;
+    //return `rgb(${Math.floor(Math.random()*51)*5}, ${Math.floor(Math.random()*51)*5}, ${Math.floor(Math.random()*51)*5})`;
 }
 
 function getRandomRect(width, height){
-    return [Math.random() *width, Math.random() * height, Math.random()*20, Math.random()*20]
+    return [
+        Math.floor(Math.random() * width), 
+        Math.floor(Math.random() * height),
+        Math.floor(Math.random() * 5) + 1, 
+        Math.floor(Math.random() * 5) + 1
+    ]
 }
 
-function getNextCtx(oldData){
-    let rectDims = getRandomRect(img_width, img_height);
-    tempCtx.fillStyle = getRandomColor();
-
-    tempCtx.width = img_width;
-    tempCtx.height = img_height;
-
+function getNextCtx(inputCtx, rect, color){
+    oldData = inputCtx.getImageData(rect[0], rect[1], rect[2], rect[3]);
+    oldDiff = compareData(oldData, goalData, rect);
     
-    tempCtx.putImageData(oldData, 0, 0);
-    
-    tempCtx.fillRect(rectDims[0], rectDims[1], rectDims[2], rectDims[3]);
 
-    newDat = tempCtx.getImageData(0, 0, img_width, img_height)
+    tempCtx.fillStyle = color;
+    tempCtx.fillRect(0, 0, rect[2], rect[3]);
 
-    return newDat;
+    newDat = tempCtx.getImageData(0, 0, rect[2], rect[3]);
+    if(compareData(newDat, goalData, rect) < oldDiff) return true;
+    return false;
 }
 
-function reverseData(data){
-    mydata = ctxIn.createImageData(img_width, img_height);
-
-    for (let i = 0; i < data.length; i++) {
-        mydata[i] = Math.abs(data[i]);
+var averageFrametime = 0;
+var updateStuffs = setInterval(function(){
+    stats.innerHTML = `Checked:${testedRectangles} Placed:${placedRectangles} Frametime:${averageFrametime}, Checks per second:${cps}`;
+    cps = 0;
+}, 1000);
+runStuff = function(){
+    var newColor = getRandomColor();
+    var newRect = getRandomRect(img_width, img_height);
+    var data = getNextCtx(ctxIn, newRect, newColor);
+    if(data){
+        placedRectangles++;
+        ctxIn.fillStyle = newColor;
+        ctxIn.fillRect(newRect[0], newRect[1], newRect[2], newRect[3]);
+    } else{
+        testedRectangles++;
     }
-
-    console.log(data)
-    return mydata;
 }
-
-var interval = setInterval(function() {
-    ctxInData = ctxIn.getImageData(0,0,img_width,img_height)
-    var data = getNextCtx(ctxInData);
-    while(compareData(data, goalData) > compareData(ctxInData, goalData)){
-        data = getNextCtx(ctxInData);
-    }
-
-    ctxIn.putImageData(data, 0, 0);
-}, 0.1);
+var cps = 0;
+var interval = setInterval(
+    function(){
+        startTime = performance.now();
+        while(performance.now() - startTime < 16){
+            runStuff();
+            cps++;
+        }
+        averageFrametime = performance.now() - startTime;
+}, 1);
